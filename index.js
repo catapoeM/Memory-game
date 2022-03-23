d874f56ae4c78e25c8a6a8377673b4092837c89e
catapoeM <catalin_poenaru95@yahoo.com>
2022-03-23 14:57:50 +0100

    init

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 function pageLoad() {
+  getImages()
   createTable();
 
-  
-
-  function createTable(par) {
-    
-    var imgArray = new Array();
+  function getImages(imgArray = new Array(), imgArrayCopy = new Array()) {
     for (let i = 1; i < 7; ++i) {
       imgArray[i] = new Image()
       imgArray[i].src = 'img/' + i + '.jpg'
+      imgArrayCopy[i] = new Image()
+      imgArrayCopy[i].src = 'imgCpy/' + i + '.jpg'
     }
+    return imgArray, imgArrayCopy
+  }
+
+  function createTable() {
+    var imgArray = getImages.call(imgArray);
+    var imgArrayCopy = getImages.call(imgArrayCopy)
     
     var table = document.getElementById('table');
-    for (var i = 0, idCell = 1; i < 3; ++i) {
+    for (var i = 0, idCell = 1, index_img = 1; i < 3; ++i) {
       var row = table.insertRow(i);     // Create an empty <tr> element and add it to the 1st position of the table
-      for (var j = 0; j < 4; ++j, ++idCell) {
+      for (var j = 0; j < 4; ++j, ++idCell, ++index_img) {
+        
         var cell = row.insertCell(j);   // Insert new cell (<td> elements) at the 1st position of the "new" <tr> element
         cell.setAttribute('id', idCell)
         cell.addEventListener("click", function(c) {
@@ -24,8 +30,15 @@ function pageLoad() {
         cell.style.borderColor = "white"
         if (idCell < 7) {
           var img = document.createElement('img')
-          var img = imgArray[idCell]
+          var img = imgArray[index_img]
           cell.appendChild(img)
+        }else if (idCell >= 7) {
+          if (idCell == 7) {
+            index_img = 1;
+          }
+          var imgC = document.createElement('img')
+          var imgC = imgArrayCopy[index_img]
+          cell.appendChild(imgC)
         }
         
       }
