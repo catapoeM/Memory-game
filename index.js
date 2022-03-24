f50a72faea06ba6c8e921f8f35022c86b1c53e60
catapoeM <catalin_poenaru95@yahoo.com>
2022-03-24 19:12:22 +0100

    init

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ function pageLoad() {
 
   function createTable() {  
     var imgArray = getImages.call(imgArray);
-    
     var table = document.getElementById('table');
-    for (var i = 0, idCell = 1, k = 0; i < 3; ++i) {
+    table.style.marginLeft = 'auto'
+    table.style.marginRight = 'auto'
+    table.style.marginTop = '1cm'
+    for (var i = 0, idCell = 0; i < 3; ++i) {
       var row = table.insertRow(i);     // Create an empty <tr> element and add it to the 1st position of the table
       for (var j = 0; j < 4; ++j, ++idCell) {
         var cell = row.insertCell(j);   // Insert new cell (<td> elements) at the 1st position of the "new" <tr> element
@@ -13,26 +15,28 @@ function pageLoad() {
         cell.addEventListener("click", function(c) {
           alert(this.id)
         });
+        cell.style.width = '6px'
+        cell.style.height = '5px'
+        cell.style.border = '2px solid'
         cell.style.backgroundColor = "black";
         cell.style.borderColor = "white"
-        if (k < 12) {
+        if (idCell < 12) {
           var img = document.createElement('img')
-          var img = imgArray[k]
+          var img = imgArray[idCell]
           cell.appendChild(img)
-          ++k;
         }
       }
     }
   }
 
-  function getImages(imgArray = new Array()) {
+  function getImages(imgArray = new Array(), imgId = new Array()) {
     var randomNumbers = mathRandom.call(randomNumbers)
-    alert("get " + randomNumbers[0])
-    alert(randomNumbers)
     for (let i = 0, j = randomNumbers[i]; i < 12; ++i, j = randomNumbers[i]) {
         imgArray[i] = new Image()
         imgArray[i].src = 'img/' + j + '.jpg'
+        imgId[i] = imgArray[i].src
     }
+    alert(imgId)
     return imgArray
   }
 
